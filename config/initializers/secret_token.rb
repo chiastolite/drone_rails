308e69ea4d918bad43e69aee61e4bcaceeffa9c9
chiastolite <chiastolite.1980@gmail.com>
2014-02-20 19:25:06 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DroneRails::Application.config.secret_key_base = 'c70f476a9d70e3f34154f5a000e4d55b5fe8268d66ca7679aa8600b923f49b7d3a1cff1c2766c1b8c043a5f7944c59db2bc8ef35969045543112661abe7eda35'
